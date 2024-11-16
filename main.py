7e171d30c006f596006e22c07a68b11bb9515bbd
Amelie-Jimbo <guillermojimbo@gmail.com>
2024-11-15 19:26:25 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+dictionary = {
+             "LOL": "Una respuesta a algo gracioso",
+             "CRINGE": "Algo raro o embarazoso",
+             "ROFL": "Una respuesta a una broma",
+             "SHEESH": "Ligera desaprobación",
+             "CREEPY": "Aterrador, siniestro",
+             "AGGRO": "Ponerse agresivo/enojado"
+    
+             } 
+word = input("Hola, escribe una palabra que no entiendas con mayúsculas: ")
+print(dictionary[word])
